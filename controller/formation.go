6f6c76c5fe4ad9fe9e44e377534cec89520d927a
Lewis Marshall <lewis@lmars.net>
2014-12-02 04:20:06 +0000

    controller: Move DB helpers into pkg/postgres
    Signed-off-by: Lewis Marshall <lewis@lmars.net>

--- controller/formation.go
+++ controller/formation.go
@@ -11,6 +11,7 @@ import (
 	"github.com/flynn/flynn/Godeps/_workspace/src/github.com/flynn/pq"
 	"github.com/flynn/flynn/Godeps/_workspace/src/github.com/flynn/pq/hstore"
 	ct "github.com/flynn/flynn/controller/types"
+	"github.com/flynn/flynn/pkg/postgres"
 )
 
 type formationKey struct {
@@ -18,7 +19,7 @@ type formationKey struct {
 }
 
 type FormationRepo struct {
-	db        *DB
+	db        *postgres.DB
 	apps      *AppRepo
 	releases  *ReleaseRepo
 	artifacts *ArtifactRepo
@@ -28,7 +29,7 @@ type FormationRepo struct {
 	subMtx        sync.RWMutex
 }
 
-func NewFormationRepo(db *DB, appRepo *AppRepo, releaseRepo *ReleaseRepo, artifactRepo *ArtifactRepo) *FormationRepo {
+func NewFormationRepo(db *postgres.DB, appRepo *AppRepo, releaseRepo *ReleaseRepo, artifactRepo *ArtifactRepo) *FormationRepo {
 	return &FormationRepo{
 		db:            db,
 		apps:          appRepo,
@@ -63,7 +64,7 @@ func (r *FormationRepo) Add(f *ct.Formation) error {
 	return nil
 }
 
-func scanFormation(s Scanner) (*ct.Formation, error) {
+func scanFormation(s postgres.Scanner) (*ct.Formation, error) {
 	f := &ct.Formation{}
 	var procs hstore.Hstore
 	err := s.Scan(&f.AppID, &f.ReleaseID, &procs, &f.CreatedAt, &f.UpdatedAt)
@@ -80,8 +81,8 @@ func scanFormation(s Scanner) (*ct.Formation, error) {
 			f.Processes[k] = n
 		}
 	}
-	f.AppID = cleanUUID(f.AppID)
-	f.ReleaseID = cleanUUID(f.ReleaseID)
+	f.AppID = postgres.CleanUUID(f.AppID)
+	f.ReleaseID = postgres.CleanUUID(f.ReleaseID)
 	return f, nil
 }
 
@@ -142,7 +143,7 @@ func (r *FormationRepo) publish(appID, releaseID string) {
 func (r *FormationRepo) expandFormation(formation *ct.Formation) (*ct.ExpandedFormation, error) {
 	app, err := r.apps.Get(formation.AppID)
 	if err == ErrNotFound {
-		app = &ct.App{ID: cleanUUID(formation.AppID)}
+		app = &ct.App{ID: postgres.CleanUUID(formation.AppID)}
 	} else if err != nil {
 		return nil, err
 	}
